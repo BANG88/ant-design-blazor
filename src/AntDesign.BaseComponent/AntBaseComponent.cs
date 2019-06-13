ee2fa247e94318750ab8bfc234a0a6b99830e50c
bang <sqibang@gmail.com>
2019-06-13 18:12:29 +0800

    feat: add button

---
+++ src/AntDesign.BaseComponent/AntBaseComponent.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntDesign.BaseComponent
+{
+    public class AntBaseComponent : ComponentBase, IDisposable
+    {
+        protected string getPrefixCls(string suffixCls)
+        {
+            string prefixCls = "ant";
+
+            return string.IsNullOrEmpty(suffixCls) ? prefixCls : $"{prefixCls}-{suffixCls}";
+        }
+        private ElementRef _ref;
+
+        public ElementRef Ref
+        {
+            get { return _ref; }
+            set { _ref = value; }
+        }
+
+        public ClassNames ClassNames { get; } = new ClassNames();
+        /// <summary>
+        /// Specifies one or more classnames for an DOM element.
+        /// </summary>
+        [Parameter]
+        public string Class
+        {
+            get => _class;
+            set
+            {
+                _class = value;
+                ClassNames.MakeDirty();
+            }
+        }
+
+
+        /// <summary>
+        /// Specifies an inline style for an DOM element.
+        /// </summary>
+        [Parameter]
+        public string Style
+        {
+            get => _style;
+            set
+            {
+                _style = value;
+                this.StateHasChanged();
+            }
+        }
+
+        private string _class;
+        private string _style;
+
+        protected AntBaseComponent()
+        {
+            ClassNames.Get(() => this.Class);
+        }
+
+        private Queue<Func<Task>> afterRenderCallQuene = new Queue<Func<Task>>();
+
+        private bool isRendered = false;
+
+        protected void CallAfterRender(Func<Task> action)
+        {
+            afterRenderCallQuene.Enqueue(action);
+        }
+        protected async virtual Task OnFirstAfterRenderAsync()
+        {
+        }
+        protected async override Task OnAfterRenderAsync()
+        {
+            if (!isRendered)
+            {
+                await OnFirstAfterRenderAsync();
+                isRendered = true;
+            }
+
+            if (afterRenderCallQuene.Count > 0)
+            {
+                var actions = afterRenderCallQuene.ToArray();
+                afterRenderCallQuene.Clear();
+
+                foreach (var action in actions)
+                {
+                    if (Disposed)
+                    {
+                        return;
+                    }
+
+                    await action();
+                }
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            Disposed = true;
+        }
+
+        protected bool Disposed { get; private set; }
+    }
+}
--- src/AntDesign.Button/AntButtonComponent.cs
+++ src/AntDesign.Button/AntButtonComponent.cs
@@ -2,11 +2,55 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using AntDesign.BaseComponent;
+using System.Threading.Tasks;
 
 namespace AntDesign.Button
 {
-    public class AntButtonComponent: ComponentBase
+
+    public static class ButtonTypes
     {
+        public const string Default = "default";
+        public const string Primary = "primary";
+        public const string Ghost = "ghost";
+        public const string Dashed = "dashed";
+        public const string Danger = "danger";
+        public const string Link = "link";
+    }
+    public class AntButtonComponent : AntBaseComponent
+    {
+        public AntButtonComponent()
+        {
+
+        }
+        protected override Task OnParametersSetAsync()
+        {
+            var prefixCls = getPrefixCls("btn");
+
+            ClassNames.Add(prefixCls).
+                Add($"{prefixCls}-{Type}");
+
+            return base.OnParametersSetAsync();
+        }
+        /// <summary>
+        /// Inline label of Button.
+        /// </summary>
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+
+        private string _type;
+
+        [Parameter]
+        public string Type
+        {
+            get { return _type ?? ButtonTypes.Default; }
+            set
+            {
+                _type = value;
+                ClassNames.MakeDirty();
+            }
+        }
 
     }
 }
