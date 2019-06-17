daf8cde1f95287dced5c457e4313d4640e1508a7
bang <sqibang@gmail.com>
2019-06-17 11:08:46 +0800

    feat: add Icon

---
+++ src/AntDesign.Icon/AntIconComponent.cs
@@ -0,0 +1,42 @@
+﻿using AntDesign.BaseComponent;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace AntDesign.Icon
+{
+    public class AntIconComponent : AntBaseComponent
+    {
+        [Parameter]
+        public string Type { get; set; }
+
+        [Parameter]
+        public bool Spin { get; set; }
+
+        [Parameter]
+        public string Width { get; set; } = "1em";
+
+        [Parameter]
+        public string Height { get; set; } = "1em";
+
+        [Parameter]
+        public string Fill { get; set; } = "currentColor";
+
+        public AntIconDefinition Icon { get; set; }
+       
+
+        protected override Task OnParametersSetAsync()
+        {
+            var icon = AntIcons.all.Find(a => a.Name.Equals(this.Type));
+            if (icon != null)
+            {
+                this.Icon = icon;
+                //this.StateHasChanged();
+            }
+            return base.OnParametersSetAsync();
+        }
+
+    }
+}
---
+++ src/AntDesign.Icon/AntIconDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+
+namespace AntDesign.Icon
+{
+    public class AntIconDefinition
+
+    {
+        public string Name { get; set; }
+        public string Theme { get; set; }
+        public string ViewBox { get; set; }
+        public List<string> Paths { get; set; }
+    }
+}
