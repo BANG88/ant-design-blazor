05672b2e3f3eae02e5afbb51ae130f96dbaf8df3
bang <sqibang@gmail.com>
2019-06-19 15:35:26 +0800

    feat: sh

--- publish.sh
+++ publish.sh
@@ -11,4 +11,4 @@ fi
 
 release="./bin/Release/"
 
-for d in ./src/*/; do (cd "$d" && rm -rf "$release" && dotnet pack --version-suffix "$version" -c Release && find "$release" -iname "*.$version.nupkg" -exec dotnet nuget push {} -k "$key" -s https://api.nuget.org/v3/index.json \; && rm -rf "$release"); done
+for d in ./src/*/; do (cd "$d" && rm -rf "$release" && dotnet pack --version-suffix "$version" -c Release && find "$release" -iname "*.$version.nupkg" -exec dotnet nuget push {} -k "$key" -s https://api.nuget.org/v3/index.json \; && rm -rf "$release" "./obj/Release/"); done
