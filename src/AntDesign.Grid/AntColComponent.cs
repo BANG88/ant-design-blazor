3bbc852f2d044cd50f05fe271715bd6f45a46ef4
bang <sqibang@gmail.com>
2019-06-19 17:58:25 +0800

    feat: row and col

--- src/AntDesign.Grid/AntColComponent.cs
+++ src/AntDesign.Grid/AntColComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AntDesign.BaseComponent;
@@ -8,31 +9,122 @@ using OneOf;
 
 namespace AntDesign.Grid
 {
-    using ColSpanType = OneOf<string, int>;
+    using AntColSpanType = OneOf<string, int>;
+
     public class AntColSize
     {
-        public ColSpanType span { get; set; }
+        public AntColSpanType span { get; set; }
+        public AntColSpanType order { get; set; }
+
+        public AntColSpanType offset { get; set; }
+
+        public AntColSpanType push { get; set; }
+
+        public AntColSpanType pull { get; set; }
+
     }
     /// <summary>
     /// Base Component for AntCol
     /// </summary>
     public class AntColComponent : AntBaseComponent
     {
-        protected OneOf<string, int> ColSpanType;
         private string prefixCls = getPrefixCls("col");
         protected override Task OnParametersSetAsync()
         {
             ClassNames.Clear()
                .Add(prefixCls)
-               .Add($"{prefixCls}-{Span.Value}", () => this.Span.Value != null);
+               .Add($"{prefixCls}-{span.Value}", () => this.span.Value != null)
+               .Add($"{prefixCls}-order-{order.Value}", () => this.order.Value != null)
+               .Add($"{prefixCls}-offset-{offset.Value}", () => this.offset.Value != null)
+               .Add($"{prefixCls}-push-{push.Value}", () => this.push.Value != null)
+               .Add($"{prefixCls}-pull-{pull.Value}", () => this.pull.Value != null)
+               ;
+
+            this.addSizeClass();
 
             return base.OnParametersSetAsync();
         }
+        private void addSizeClass()
+        {
+            string[] sizes = { "xs", "sm", "md", "lg", "xl", "xxl" };
+            var fileds = typeof(AntColComponent).GetFields().ToList();
+            foreach (var size in sizes)
+            {
+                AntColSize antColSize = new AntColSize();
+                var maybeXs = fileds.Find(f => f.Name.Equals(size));
 
+                if (maybeXs == null)
+                {
+                    continue;
+                }
+
+                var oneOfX = (OneOf<AntColSpanType, AntColSize>)maybeXs.GetValue(typeof(AntColComponent));
+
+                oneOfX.Switch(cspan =>
+                {
+                    antColSize.span = cspan;
+                }, csize =>
+                {
+                    antColSize = csize;
+                });
+
+                ClassNames
+                    .Add($"{prefixCls}-{size}-{antColSize.span}", () => antColSize.span.Value != null)
+                    .Add($"{prefixCls}-{size}-order-{antColSize.order}", () => antColSize.order.Value != null)
+                    .Add($"{prefixCls}-{size}-offset-{antColSize.offset}", () => antColSize.offset.Value != null)
+                    .Add($"{prefixCls}-{size}-push-{antColSize.push}", () => antColSize.push.Value != null)
+                    .Add($"{prefixCls}-{size}-pull-{antColSize.pull}", () => antColSize.pull.Value != null)
+                    ;
+            }
+        }
+        /// <summary>
+        /// get style if gutter provide
+        /// </summary>
+        /// <returns></returns>
+        protected string getStyle()
+        {
+            var style = "";
+            if (this.gutter > 0)
+            {
+                style = $"padding-left: {this.gutter / 2}px;padding-right: {this.gutter / 2}px;";
+            }
+            return $"{style} {this.Style}".Trim();
+        }
 
         [Parameter]
-        public ColSpanType Span { get; set; }
+        public AntColSpanType span { get; set; }
 
+        [Parameter]
+        public AntColSpanType order { get; set; }
+
+        [Parameter]
+        public AntColSpanType offset { get; set; }
+
+        [Parameter]
+        public AntColSpanType push { get; set; }
+
+        [Parameter]
+        public AntColSpanType pull { get; set; }
+
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> xs { get; set; }
+
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> sm { get; set; }
+
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> md { get; set; }
+
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> lg { get; set; }
+
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> xl { get; set; }
+        [Parameter]
+        public OneOf<AntColSpanType, AntColSize> xxl { get; set; }
+
+        [CascadingParameter]
+        public int gutter { get; set; }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
--- src/AntDesign.Grid/AntRowComponent.cs
+++ src/AntDesign.Grid/AntRowComponent.cs
@@ -8,25 +8,60 @@ using OneOf;
 
 namespace AntDesign.Grid
 {
+
+    public static class AntRowAligns
+    {
+        public const string Top = "top";
+        public const string Middle = "middle";
+        public const string Bottom = "bottom";
+    }
+
+    public static class AntRowJustify
+    {
+        public const string Start = "start";
+        public const string End = "end";
+        public const string Center = "center";
+        public const string SpaceAround = "space-around";
+        public const string SpaceBetween = "space-between";
+    }
+
     /// <summary>
     /// Base Component for AntRow
     /// </summary>
     public class AntRowComponent : AntBaseComponent
     {
-        protected OneOf<string, int> ColSpanType;
+
         private string prefixCls = getPrefixCls("row");
         protected override Task OnParametersSetAsync()
         {
             ClassNames.Clear()
                .Add(prefixCls)
-               .Add($"{prefixCls}-{Type}", () =>!string.IsNullOrEmpty(Type));
+               .Add($"{prefixCls}-{Type}", () => hasType)
+               .Add($"{prefixCls}-{Type}-{Justify}", () => hasType && !string.IsNullOrEmpty(Justify))
+               .Add($"{prefixCls}-{Type}-{Align}", () => hasType && !string.IsNullOrEmpty(Align))
+               ;
 
             return base.OnParametersSetAsync();
         }
 
+        /// <summary>
+        /// check type 
+        /// </summary>
+        private bool hasType => !string.IsNullOrEmpty(Type);
+
         [Parameter]
         public string Type { get; set; } = "flex";
+
+        [Parameter]
+        public string Align { get; set; }
+
+        [Parameter]
+        public string Justify { get; set; }
+
+        [Parameter]
+        public int Gutter { get; set; }
         [Parameter]
         public RenderFragment ChildContent { get; set; }
+
     }
 }
