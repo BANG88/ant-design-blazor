40c961b0f348ad91770cd555f788dc562579cad1
bang <sqibang@gmail.com>
2019-06-20 10:45:07 +0800

    feat: add layout

---
+++ src/AntDesign.Layout/AntLayoutComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// Base Component for AntLayout
+    /// </summary>
+    public class AntLayoutComponent : AntBaseComponent
+    {
+        private string prefixCls = getPrefixCls("layout");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls);
+
+            return base.OnParametersSetAsync();
+        }
+    }
+}
---
+++ src/AntDesign.Layout/README.md
@@ -0,0 +1,24 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Layout.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Layout)
+
+
+# Layout
+
+> [https://ant.design/components/layout/](https://ant.design/components/layout/)
+
+## Usage
+
+### Install
+
+```sh
+dotnet add package AntDesign.Layout
+```
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntLayout />
+
+```
