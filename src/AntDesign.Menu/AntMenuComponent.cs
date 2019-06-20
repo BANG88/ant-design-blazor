2fe47378b8ac7037c22b3cda5a353a8a66212111
bang <sqibang@gmail.com>
2019-06-20 16:47:37 +0800

    feat: add menu

---
+++ src/AntDesign.Menu/AntMenuComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// Base Component for AntMenu
+    /// </summary>
+    public class AntMenuComponent : AntBaseComponent
+    {
+        private string prefixCls = getPrefixCls("menu");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls);
+
+            return base.OnParametersSetAsync();
+        }
+    }
+}
---
+++ src/AntDesign.Menu/README.md
@@ -0,0 +1,24 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Menu.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Menu)
+
+
+# Menu
+
+> [https://ant.design/components/menu/](https://ant.design/components/menu/)
+
+## Usage
+
+### Install
+
+```sh
+dotnet add package AntDesign.Menu
+```
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntMenu />
+
+```
