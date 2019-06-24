b7b25db012b19924a83fb82a35ed6ee2d0061e0a
bang <sqibang@gmail.com>
2019-06-24 17:52:09 +0800

    feat: sider

--- src/AntDesign.Layout/AntSiderComponent.cs
+++ src/AntDesign.Layout/AntSiderComponent.cs
@@ -15,12 +15,39 @@ namespace AntDesign
         protected override Task OnParametersSetAsync()
         {
             ClassNames.Clear()
-               .Add(prefixCls);
+               .Add(prefixCls)
+               .Add($"{prefixCls}-collapsed", () => this.collapsed)
+               ;
 
             return base.OnParametersSetAsync();
         }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
+
+
+        [Parameter]
+        public string width { get; set; } = "200px";
+
+        [Parameter]
+        public string collapsedWidth { get; set; } = "80px";
+
+
+        [Parameter]
+        public bool defaultCollapsed { get; set; }
+
+        [Parameter]
+        public bool collapsed { get; set; }
+        [Parameter]
+        public bool collapsible { get; set; }
+
+        protected string getStyle()
+        {
+            var s = $"{Style} flex: 0 0 {this.siderWidth};max-width:{this.siderWidth};min-width:{this.siderWidth};width:{this.siderWidth};";
+            return s;
+        }
+        public string siderWidth => this.collapsed ? this.collapsedWidth : this.width;
+
+
     }
 }
