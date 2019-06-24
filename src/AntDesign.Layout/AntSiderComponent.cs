e499344c34811aa60f84eac15482c71c8d91edd7
bang <sqibang@gmail.com>
2019-06-24 16:45:33 +0800

    feat: layout

---
+++ src/AntDesign.Layout/AntSiderComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// Base Component for AntContent
+    /// </summary>
+    public class AntSiderComponent : AntBaseComponent
+    {
+        protected string prefixCls = getPrefixCls("layout-sider");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls);
+
+            return base.OnParametersSetAsync();
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
