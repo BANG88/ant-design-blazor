ba6d237a7be8ce2cb7eb20e49ce4d88983c33b1a
bang <sqibang@gmail.com>
2019-07-02 12:06:59 +0800

    feat: switch

---
+++ src/AntDesign.Switch/AntSwitchComponent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+
+
+    /// <summary>
+    /// Base Component for AntSwitch
+    /// </summary>
+    public class AntSwitchComponent : AntBaseComponent
+    {
+        protected string prefixCls = getPrefixCls("switch");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls)
+                .Add($"{prefixCls}-checked", () => Checked.GetValueOrDefault())
+               .Add($"{prefixCls}-disabled", () => disabled || loading)
+               .Add($"{prefixCls}-loading", () => loading)
+               .Add($"{prefixCls}-small", () => size.Equals("small"))
+               ;
+
+            return base.OnParametersSetAsync();
+        }
+
+        private bool? @checked;
+
+        [Parameter]
+        public bool defaultChecked { get; set; }
+
+
+        [Parameter]
+        public bool? Checked
+        {
+            get => @checked != null ? @checked : defaultChecked;
+            set
+            {
+                @checked = value;
+            }
+        }
+
+        [Parameter]
+        public bool disabled { get; set; }
+
+
+
+        [Parameter]
+        public bool loading { get; set; }
+
+        [Parameter]
+        public string size { get; set; } = "default";
+        [Parameter]
+        public RenderFragment CheckedChildren { get; set; }
+
+
+
+        [Parameter]
+        public RenderFragment UnCheckedChildren { get; set; }
+
+        protected void OnClickHandler(UIMouseEventArgs ev)
+        {
+            if (disabled || loading)
+            {
+                return;
+            }
+            this.Checked = !this.Checked;
+            OnChange.InvokeAsync(this.Checked.GetValueOrDefault());
+        }
+        [Parameter]
+        protected EventCallback<bool> OnChange { get; set; }
+    }
+}
---
+++ src/AntDesign.Switch/README.md
@@ -0,0 +1,25 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Switch.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Switch)
+
+
+# Switch
+
+> [https://ant.design/components/switch/](https://ant.design/components/switch/)
+
+
+## Install
+
+```sh
+dotnet add package AntDesign.Switch
+```
+
+### Usage
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntSwitch />
+
+```
