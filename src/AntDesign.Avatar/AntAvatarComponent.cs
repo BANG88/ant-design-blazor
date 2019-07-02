8d8094d5bc8f46ed7373b6049f0746f05ac452b9
bang <sqibang@gmail.com>
2019-07-02 16:53:06 +0800

    feat: avatar

---
+++ src/AntDesign.Avatar/AntAvatarComponent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// Base Component for AntAvatar
+    /// </summary>
+    public class AntAvatarComponent : AntBaseComponent
+    {
+        protected string prefixCls = getPrefixCls("avatar");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls)
+               .Add($"{prefixCls}-lg", () => size.Equals("large"))
+               .Add($"{prefixCls}-sm", () => size.Equals("small"))
+               .Add($"{prefixCls}-{shape}", () => !string.IsNullOrEmpty(shape))
+               .Add($"{prefixCls}-image", () => !string.IsNullOrEmpty(src))
+               .Add($"{prefixCls}-icon", () => !string.IsNullOrEmpty(icon))
+               ;
+
+            return base.OnParametersSetAsync();
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string src { get; set; }
+
+        [Parameter]
+        public string srcSet { get; set; }
+        [Parameter]
+        public string alt { get; set; }
+        [Parameter]
+        public string icon { get; set; }
+        /// <summary>
+        /// 'circle' | 'square'
+        /// </summary>
+        [Parameter]
+        public string shape { get; set; } = "circle";
+
+        /// <summary>
+        /// 'large' | 'small' | 'default'
+        /// </summary>
+        [Parameter]
+        public string size { get; set; } = "default";
+
+        
+    }
+}
---
+++ src/AntDesign.Avatar/README.md
@@ -0,0 +1,25 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Avatar.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Avatar)
+
+
+# Avatar
+
+> [https://ant.design/components/avatar/](https://ant.design/components/avatar/)
+
+
+## Install
+
+```sh
+dotnet add package AntDesign.Avatar
+```
+
+### Usage
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntAvatar />
+
+```
