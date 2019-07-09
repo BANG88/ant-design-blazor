48baaedadbc810bfe666e58eaac06d7e8d738bfb
bang88 <sqibang@gmail.com>
2019-07-09 14:58:18 +0800

    feat: add input

---
+++ src/AntDesign.Input/AntInputComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// Base Component for AntInput
+    /// </summary>
+    public class AntInputComponent : AntBaseComponent
+    {
+        protected string prefixCls = getPrefixCls("input");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls);
+
+            return base.OnParametersSetAsync();
+        }
+    }
+}
---
+++ src/AntDesign.Input/README.md
@@ -0,0 +1,25 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Input.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Input)
+
+
+# Input
+
+> [https://ant.design/components/input/](https://ant.design/components/input/)
+
+
+## Install
+
+```sh
+dotnet add package AntDesign.Input
+```
+
+### Usage
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntInput />
+
+```
