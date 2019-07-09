998ea6478add2f6a34a95991f11c4e1d49532b7b
bang88 <sqibang@gmail.com>
2019-07-09 16:46:14 +0800

    feat: add addon for input

--- src/AntDesign.Input/AntInputComponent.cs
+++ src/AntDesign.Input/AntInputComponent.cs
@@ -25,6 +25,7 @@ namespace AntDesign
         {
             ClassNames classNames = new ClassNames();
             classNames
+            .Add(prefixCls)
             .Add($"{prefixCls}-sm", () => size.Equals("small"))
             .Add($"{prefixCls}-lg", () => size.Equals("large"))
             .Add($"{prefixCls}-disabled", () => disabled)
@@ -32,10 +33,40 @@ namespace AntDesign
 
             return classNames.Class;
         }
+        protected string wrapperClassName
+        {
+            get => $"{this.prefixCls}-group";
+        }
 
+        protected string addonClassName
+        {
+            get => $"{wrapperClassName}-addon";
+        }
+
+        protected string getMergedGroupClassName()
+        {
+            ClassNames classNames = new ClassNames();
+            classNames
+            .Add($"{prefixCls}-group-wrapper")
+            .Add($"{prefixCls}-group-wrapper-sm", () => size.Equals("small"))
+            .Add($"{prefixCls}-group-wrapper-lg", () => size.Equals("large"))
+            ;
+
+            return $"{Class} {classNames.Class}".Trim();
+        }
+        protected string getMergedWrapperClassName()
+        {
+            ClassNames classNames = new ClassNames();
+            classNames
+            .Add($"{prefixCls}-wrapper")
+            .Add($"{wrapperClassName}", () => AddonBefore != null || AddonAfter != null)
+            ;
+
+            return $"{classNames.Class}".Trim();
+        }
         protected string getInputClass()
         {
-            return $"{Class} {this.getInputClassName()}".Trim();
+            return $"{this.getInputClassName()}".Trim();
         }
         [Parameter]
         public string size { get; set; } = "default";
@@ -47,5 +78,16 @@ namespace AntDesign
         public string value { get; set; }
         [Parameter]
         public string placeholder { get; set; }
+
+
+        [Parameter]
+        public RenderFragment AddonBefore { get; set; }
+
+        [Parameter]
+        public RenderFragment AddonAfter { get; set; }
+        [Parameter]
+        public RenderFragment Prefix { get; set; }
+        [Parameter]
+        public RenderFragment Suffix { get; set; }
     }
 }
