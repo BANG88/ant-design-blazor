e66ef134c71ef1aa719daecd5c84d735e214177c
bang <sqibang@gmail.com>
2019-07-11 09:59:37 +0800

    feat: textarea

--- src/AntDesign.Input/AntTextAreaComponent.cs
+++ src/AntDesign.Input/AntTextAreaComponent.cs
@@ -11,13 +11,25 @@ namespace AntDesign
     /// </summary>
     public class AntTextAreaComponent : AntBaseComponent
     {
-        protected string prefixCls = getPrefixCls("textarea");
+        protected string prefixCls = getPrefixCls("input");
         protected override Task OnParametersSetAsync()
         {
             ClassNames.Clear()
-               .Add(prefixCls);
+               .Add(prefixCls)
+               .Add($"{prefixCls}-disabled", () => disabled)
+               ;
 
             return base.OnParametersSetAsync();
         }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string placeholder { get; set; }
+
+        [Parameter]
+        public bool disabled { get; set; }
+        [Parameter]
+        protected EventCallback<UIChangeEventArgs> OnChange { get; set; }
     }
 }
