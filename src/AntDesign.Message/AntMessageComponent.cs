e6ca1448ca0db95dbf2578b058cbe1aba445b7b7
bang <sqibang@gmail.com>
2019-07-29 17:19:35 +0800

    feat: WIP message

---
+++ src/AntDesign.Message/AntMessageComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public class MessageType
+    {
+        public const string info = "info";
+        public const string success = "success";
+        public const string error = "error";
+        public const string warning = "warning";
+        public const string loading = "loading";
+    }
+    /// <summary>
+    /// Base Component for AntMessage
+    /// </summary>
+    public class AntMessageComponent : AntBaseComponent
+    {
+        protected string prefixCls = getPrefixCls("message");
+        protected override Task OnParametersSetAsync()
+        {
+            ClassNames.Clear()
+               .Add(prefixCls);
+
+            return base.OnParametersSetAsync();
+        }
+    }
+}
---
+++ src/AntDesign.Message/README.md
@@ -0,0 +1,25 @@
+[![NuGet](https://img.shields.io/nuget/v/AntDesign.Message.svg?style=flat-square&color=green)](https://www.nuget.org/packages/AntDesign.Message)
+
+
+# Message
+
+> [https://ant.design/components/message/](https://ant.design/components/message/)
+
+
+## Install
+
+```sh
+dotnet add package AntDesign.Message
+```
+
+### Usage
+
+```cs
+@using AntDesign
+```
+
+```razor
+
+<AntMessage />
+
+```
